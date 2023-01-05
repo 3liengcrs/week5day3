84fe0b91c2b73ca5976a95123fc1949e0e7feb2e
Ali <48534054+3liengcrs@users.noreply.github.com>
2023-01-05 12:16:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,37 +12,105 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const randomQuote = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
+
   randomQuote();
+
+
   
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
+  let mainTitle = document.querySelector("#main-title");
+  mainTitle.textContent = "The New Title";
 
 
   // Part 2
+  let bodyStyle = document.querySelector("body").style.backgroundColor = "red";
 
 
   // Part 3
+ let list = document.querySelector("#favorite-things");
+ list.removeChild(list.lastElementChild);
+
+
 
 
   // Part 4
+  let specialTitle = document.querySelectorAll("#special-title");
+specialTitle.forEach(title =>{
+  title.style.fontSize='2rem'
+});
+
 
 
   // Part 5
-
+  let listOfCity = document.querySelector("#past-races");
+  document.querySelectorAll('#past-races > li').forEach((item) =>{
+    if (item.textContent.trim() === "Chicago"){
+      document.querySelector("#past-races").removeChild(item);
+    }
+  })
+  
+  
+  
 
   // Part 6
+  let el = document.createElement('li');
+  el.innerHTML = "New York";
+  listOfCity.appendChild(el);
 
 
   // Part 7
+  // let newYork = document.createElement('div');
+
+  // let races = document.querySelector('#past-races')
+  // newYork.innerHTML = '<p>The journey of a thousand miles begins with a single step.👣</p>';
+//   newYork.className = 'blog-post'
+//  specialTitle.appendChild(newYork);
+
+
+//Create new div with class of .blog-post
+let blogPost = document.createElement('div');
+blogPost.className = 'blog-post purple';
+let main = document.querySelector('.main')
+
+//Create new h2 with text 
+let postTitle = document.createElement('h1');
+postTitle.innerText = 'New York';
+
+//Create new p with some text 
+let postText = document.createElement('p');
+postText.innerText = 'The journey of a thousand miles begins with a single step.👣'
+//Append elements in order 
+blogPost.appendChild(postTitle); 
+blogPost.appendChild(postText); 
+main.appendChild(blogPost);
 
 
   // Part 8
+  let fun = document.querySelector('#quote-title').addEventListener("click", randomQuote);
 
 
   // Part 9
 
+// let selBlogPost = document.querySelectorAll('.blog-post');
+// selBlogPost.forEach(elem => {
+
+// })
 
 
-
+// });
+document.querySelectorAll('.blog-post').forEach((item) => {
+  item.addEventListener('mouseout', (e) => {
+    e.currentTarget.classList.toggle("purple");
+  });
+  item.addEventListener('mouseenter', (e) => {
+    e.stopPropagation();
+    e.currentTarget.classList.toggle("red");
+  });
+  });
 });
+
+
+
+
